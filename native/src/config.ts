868c1ad309e648298e4e6205709bdd7b97de3b47
Naveen-145119 <naveenreddy95190@gmail.com>
2026-09-11 12:13:38 +0530

    chore(native): set Google web client id

--- native/src/config.ts
+++ native/src/config.ts
@@ -18,5 +18,5 @@ export const config: AppConfig = {
   supabaseAnonKey: 'sb_publishable_N-I0xWkc2SXY6kga0iD0_Q_awDjKXNr',
   betaMode: true,
   betaChatLimit: 30,
-  googleWebClientId: '',
+  googleWebClientId: '191112449176-o48v15ln9geb6i0rev1g8h50tekph5hu.apps.googleusercontent.com',
 };
